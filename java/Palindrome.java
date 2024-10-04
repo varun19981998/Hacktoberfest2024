c0c42b93d37c4ee2f971b0ca8744947eb647a829
varun pratap singh <84224041+varun19981998@users.noreply.github.com>
2024-10-04 09:29:48 +0530

    Update Palindrome.java

--- java/Palindrome.java
+++ java/Palindrome.java
@@ -1,41 +1,28 @@
-import java.util.Scanner;
+public class Solution {
 
-public class Palindrome {
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
+  public boolean isPalindrome(String s) {
+    for (int low = 0, high = s.length() - 1;; low++, high--) {
+      char cLow = 0, cHigh = 0;
 
-        
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+      // Find the next acceptable character for the increasing index.
+      while (low < high && !Character.isLetterOrDigit(cLow = s.charAt(low))) {
+        low++;
+      }
 
-        
-        scanner.close();
+      // Find the previous acceptable character for the decreasing index.
+      while (low < high && !Character.isLetterOrDigit(cHigh = s.charAt(high))) {
+        high--;
+      }
 
-        
-        String numberStr = Integer.toString(number);
-
-        
-        if (isPalindrome(numberStr)) {
-            System.out.println(number + " is a palindrome.");
-        } else {
-            System.out.println(number + " is not a palindrome.");
-        }
-    }
-
-    
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
+      if (low >= high) {
+        // All previous character comparisons succeeded and we have a palindrome.
         return true;
+      }
+
+      if (Character.toUpperCase(cLow) != Character.toUpperCase(cHigh)) {
+        // This is not a palindrome.
+        return false;
+      }
     }
+  }
 }
